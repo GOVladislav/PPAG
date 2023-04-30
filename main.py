e12ac88de9b5cdfae47a3a0076563581eb19ac5c
Vladislav <gvsnbala@gmail.com>
2023-04-30 11:59:54 +0800

    feat: Установил логирование через loguru

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+from loguru import logger
+
 from config import setup_config
 from PPC.PPCommerial import PCSVFile
 from utils import find_csv, find_ymal
 
 
 def main():
+    logger.add('debug.log', format='{time} {level} {message}',
+               level='DEBUG', rotation='1 day', compression='zip',
+               )
+
     config = setup_config(find_ymal())
+
     csv_file = PCSVFile(
         path_file=find_csv(),
         percent=float(config.percent),
